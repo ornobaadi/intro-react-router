9847ebe782b625f7c1042ab74991372c306afdd0
ornobaadi <aadi4789@gmail.com>
2024-10-30 18:43:40 +0600

    Nested route

---
+++ src/components/About/About.jsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni error cumque ipsam deserunt repellendus ea excepturi, quo expedita alias est.</p>
+        </div>
+    );
+};
+
+export default About;
